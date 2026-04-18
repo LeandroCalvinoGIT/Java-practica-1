b0f07700ecd33d8af57150259ebfe8cb3803042c
LeandroCalvinoGITutn <lecalvino@frba.utn.ar>
2026-04-18 00:15:30 -0300

    #0.0.0 - Inital commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main{
+
+    public static void main(String[] args){
+
+        // Mi primer programa de java!
+        System.out.println("Hola amigos de youtube");
+        System.out.println("hoy vamos a aprender java para conseguir trabajo!!");
+
+        //con sout se autogenera la funcion para print
+        System.out.println("xd");
+
+        // VARIABLES
+        // Hay de tipos Primitivas
+
+        int age = 21;
+        double height = 1.86;
+        char initial = 'L';
+        boolean isStudent = true;
+
+        if (!(isStudent)){
+            System.out.println("Tengo " + age + " y mido " + height + "m.");
+        }
+
+        // Tipo Reference
+
+        String name = "Leandro";
+        String food = "Fatays";
+
+        System.out.println("Me llamo " + name);
+        System.out.println("Me gustan las " + food);
+
+        //Input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa tu nombre: ");
+        name = scanner.nextLine();
+
+
+        System.out.println(name);
+
+        scanner.close();
+    }
+}
